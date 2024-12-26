7b496b2e2723ba91d0b1293512d685c9d24faa31
BarevAp <kmirzoyan001@gmail.com>
2024-12-26 15:10:14 +0400

    Final version

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,10 +2,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Test {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         Verification verification = new Verification(driver);
         String workspaceName = "Selenium workspace";
 
@@ -20,7 +25,9 @@ public class Test {
 
             verification.isIconCreated("//ul[@class]/li");
 
-            WebElement name = driver.findElement(By.xpath("//p[contains(@class, 'typo-small-medium')][following-sibling::ul]"));
+            WebElement name = wait.until(ExpectedConditions.presenceOfElementLocated(
+                    By.xpath("//p[contains(@class, 'typo-small-medium')][following-sibling::ul]")
+            ));
             verification.verifyWorkspaceName(name, workspaceName);
 
             workspace.deleteWorkspace(workspaceName);
